89017a9db6577d86060dbbca7cc0dd9c9325ef0b
Alejandro Cancelos <acancelos@sofrecom.com.ar>
2022-01-19 16:40:35 -0300

    Test

---
+++ TestMSTest/Mocks/ConfigurationMock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+
+namespace TestMSTest.Mocks
+{
+    class ConfigurationMock : IConfigurationSection
+    {
+        public string this[string key] { get => "llave de prueba"; set => throw new NotImplementedException(); }
+
+        public string Key => throw new NotImplementedException();
+
+        public string Path => throw new NotImplementedException();
+
+        public string Value { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IEnumerable<IConfigurationSection> GetChildren()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChangeToken GetReloadToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConfigurationSection GetSection(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestMSTest/PruebasUnitarias/EncriptacionTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using AutenticacionApiSinIdentity.Servicios;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TestMSTest.Mocks;
+
+namespace TestMSTest.PruebasUnitarias
+{
+    [TestClass]
+    public class EncriptacionTests
+    {
+        [TestMethod]
+        public void DevuelveCadenaEncriptada()
+        {
+
+
+
+
+
+            //Arrange
+            var inMemorySettings = new Dictionary<string, string> {
+                {"Llave", "Lllave de Test"}};
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+
+
+            var encriptar = new Encriptacion(configuration);
+            string cadena = "Cadena a encriptar";
+
+            var resultado = encriptar.Encriptar(cadena);
+
+            Assert.AreEqual("ITaS6dqSPAWpqQQKgeh6cPtX59YOKLRX", resultado);
+
+        }
+
+        
+    }
+}
